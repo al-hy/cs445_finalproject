bcc14da809273317810fb25ca0794c2f60381386
Adrian Hy <ahy@b01-aruba-authenticated-10-110-205-48.wlan.cpp.edu>
2018-05-02 12:02:08 -0700

    Created file structure for project

---
+++ src/cs445_finalproject/MineCraft.java
@@ -0,0 +1,16 @@
+/**
+ * Adrian Hy
+ * Jerahmeel Calma
+ * Awais Ibrahim
+ */
+
+public class MineCraft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
