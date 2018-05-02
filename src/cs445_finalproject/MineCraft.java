15fe9f4b6756256ad01d56b6513e01df234c93d9
Jerahmeel Calma <jmcalma@cpp.edu>
2018-05-02 13:10:37 -0700

    added comments

--- src/cs445_finalproject/MineCraft.java
+++ src/cs445_finalproject/MineCraft.java
@@ -1,8 +1,16 @@
-/**
- * Adrian Hy
- * Jerahmeel Calma
- * Awais Ibrahim
- */
+/***************************************************************
+* file: Minecraft.java
+* author: Adrian Hy, Jerahmeel Calma, Awais Ibrahim
+* class: CS 445 – Computer Graphics
+*
+* assignment: Final Project
+* date last modified: 05/02/18
+*
+* purpose: Creates a 640 x 480 window that allows the user to 
+* view a cube with six different colored sides. The user can move
+* the camera using the mouse and keyboard.
+*
+****************************************************************/
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
@@ -12,8 +20,10 @@ import org.lwjgl.util.glu.GLU;
 
 public class MineCraft {
     private FPCameraController fp = new FPCameraController(0f,0f,0f);
-    private DisplayModedisplayMode;
+    private DisplayMode displayMode;
     
+    //method: start
+    //purpose: calls methods to set up the window for the program
     public void start() {
         try {
             createWindow();
@@ -23,21 +33,28 @@ public class MineCraft {
             e.printStackTrace();
         }
     }
-
+    
+    //method: createWindow
+    //purpose: disables fullscreen; sets the size of the window; sets the 
+    // title of the window; creates the window for the program
     private void createWindow() throws Exception{
         Display.setFullscreen(false);
         DisplayMode d[] = Display.getAvailableDisplayModes();
-        for(int i= 0; i< d.length; i++) {
+        for(int i = 0; i < d.length; i++) {
             if(d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32) { 
                 displayMode= d[i];
                 break;
             }
         }
         Display.setDisplayMode(displayMode); 
-        Display.setTitle("Hey Mom! I am using”+ “OpenGL!!!");
+        Display.setTitle("Bootleg Minecraft lol");
         Display.create();
     }
     
+    //method: initGL
+    //purpose: sets the window's background color to black; loads the camera
+    // to view what is being displayed on the window; loads the identity
+    // matrix; set the scene to model view; provides some rendering hints
     private void initGL() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
@@ -47,6 +64,8 @@ public class MineCraft {
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     }
     
+    //method: main
+    //purpose: creates an object for the program; allows the program to run
     public static void main(String[] args) {
         Basic3D basic = new Basic3D();
         basic.start();
--- src/cs445_finalproject/Vector3Float.java
+++ src/cs445_finalproject/Vector3Float.java
@@ -1,3 +1,14 @@
+/***************************************************************
+* file: Vector3Float.java
+* author: Adrian Hy, Jerahmeel Calma, Awais Ibrahim
+* class: CS 445 – Computer Graphics
+*
+* assignment: Final Project
+* date last modified: 05/02/18
+*
+* purpose: An object to hold our camera’s position in 3D space.
+*
+****************************************************************/
 
 public class Vector3Float {
     public float x, y, z;
