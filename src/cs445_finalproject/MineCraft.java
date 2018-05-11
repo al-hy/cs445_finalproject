9cc7234e677cd3108e53b74ece4046a712ba047e
ahy <ahy@b01-aruba-authenticated-10-110-205-48.wlan.cpp.edu>
2018-05-10 22:16:52 -0700

    Modified Chunk.java to render Texture

--- src/cs445_finalproject/MineCraft.java
+++ src/cs445_finalproject/MineCraft.java
@@ -67,6 +67,8 @@ public class MineCraft {
         glEnableClientState(GL_VERTEX_ARRAY);
         glEnableClientState(GL_COLOR_ARRAY);
         glEnable(GL_DEPTH_TEST);
+        glEnable(GL_TEXTURE_2D);
+        glEnableClientState (GL_TEXTURE_COORD_ARRAY);
     }
     
     //method: main
